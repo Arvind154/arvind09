ab0825712892e152a4da63592efa6d229d9d5636
Arvind154 <aj627823@gmail.com>
2025-04-22 22:25:05 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -34,20 +34,7 @@ public class Function {
 
 //    to check if a number is prime or not
 
-public static boolean isPrime(int n) {
-    if (n <= 1) 
-        return false; // Not prime
-    for (int i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i == 0) {
-            return true; // Not prime
-        } if ( n % 2 == 0) {
-            return false; // Not prime
-        }
-    }
-    
-    }
-}
-           public static void main(String[] args) {
+
         
        }
 //     
