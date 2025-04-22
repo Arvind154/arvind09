ebe8d39b855b377b8441e3ba00d48df6d9783697
Arvind154 <aj627823@gmail.com>
2025-04-22 22:28:20 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 public class Function {
     
-    // n number of  factorial;
+    n number of  factorial;
     
     
-    // public static void printFactorial (int n) {
-    //     int fact = 1;
-    //     for (int i = 1; i <= n; i++) {
-    //         fact *= i;
-    //     }
-    //     System.out.println(fact);
-    // }
-    // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
-    //     int n = sc.nextInt();
-    //     printFactorial(n);
-      
+    public static void printFactorial (int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFactorial(n);
+    }
+}
 //   Calculate the product of two numbers
 // public static void printCalculateProduct(int a, int b) {
 //     int product = a * b;
